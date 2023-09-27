1620be265cd09bfe3cd37865fa14dcd75e72823a
심혜수 <hesushim@gmail.com>
2023-09-27 15:37:35 +0900

    FEAT: globalStyle 생성

---
+++ src/styles/globalColors.ts
@@ -0,0 +1,12 @@
+//
+// global하게 쓰이는 colors 모아놓은 파일
+//
+
+export const colors = {
+    dark: '#333F50',
+    blue:'#649EFF',
+    gray:'#DADADA',
+
+    black: '#000000',
+    white: '#FFFFFF',
+};
---
+++ src/styles/globalFonts.ts
@@ -0,0 +1,13 @@
+//
+// global하게 쓰이는 fonts 모아놓은 파일
+//
+
+export const fonts = {
+    thin: 'Inter_100Thin',
+    light: 'Inter_300Light',
+    regular: 'Inter_400Regular',
+    medium: 'Inter_500Medium',
+    semiBold:'Inter_600SemiBold',
+    bold: 'Inter_700Bold',
+    black: 'Inter_900Black',
+};
